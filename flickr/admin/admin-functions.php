25cc06af3329fa96ca05f851399d83b3dba64205
Carlos Vinicius <835129+cvscarlos@users.noreply.github.com>
2014-11-09 22:48:36 -0200

    adiciona post customizado e integra com a taxonomia

--- flickr/admin/admin-functions.php
+++ flickr/admin/admin-functions.php
@@ -1,20 +1,22 @@
 <?php
+// ícones: https://www.iconfinder.com/iconsets/fugue
+
 include_once get_template_directory() . "/helpers/rain.tpl.class.php";
 raintpl::configure("tpl_dir", get_template_directory() . "/tpl/admin/");
 raintpl::configure("cache_dir", get_template_directory() . "/tmp/");
-raintpl::configure( 'path_replace', false );
+raintpl::configure( "path_replace", false );
 
 function setup_theme_admin_menus() {
-	add_menu_page('Flickr Theme Config', 'Flickr Config', 'manage_options', 'vs_Flickr_Theme', 'theme_settings_page', get_template_directory_uri() . '/admin/config16.png');
-	add_submenu_page('vs_Flickr_Theme', 'Flickr Theme Config', 'Front Page', 'manage_options', 'vs_Flickr_Theme', 'themeConfigPage');
-	add_submenu_page('', 'Flickr Theme Config', 'Front Page', 'manage_options', 'vs_Flickr_Theme_iframe', 'themeConfigPageFrame');
+	add_menu_page("Flickr Theme Config", "Flickr Config", "manage_options", "vs_Flickr_Theme", "theme_settings_page", get_template_directory_uri() . "/admin/config16.png");
+	add_submenu_page("vs_Flickr_Theme", "Flickr Theme Config", "Front Page", "manage_options", "vs_Flickr_Theme", "themeConfigPage");
+	add_submenu_page("", "Flickr Theme Config", "Front Page", "manage_options", "vs_Flickr_Theme_iframe", "themeConfigPageFrame");
 }
 
 // Página de configuração do thema
 function themeConfigPage() {
 	// Check that the user is allowed to update options
-	if (!current_user_can('manage_options'))
-		wp_die('Você não tem permissões suficientes para acessar esta página.');
+	if (!current_user_can("manage_options"))
+		wp_die("Você não tem permissões suficientes para acessar esta página.");
 
 	$tpl = new raintpl();
 	$tpl->draw("config");
@@ -22,8 +24,8 @@ function themeConfigPage() {
 // Iframe da página de configuração do iframe
 function themeConfigPageFrame() {
 	// Check that the user is allowed to update options
-	if (!current_user_can('manage_options'))
-		wp_die('Você não tem permissões suficientes para acessar esta página.');
+	if (!current_user_can("manage_options"))
+		wp_die("Você não tem permissões suficientes para acessar esta página.");
 
 	$tpl = new raintpl();
 	$tpl->assign(array(
@@ -36,13 +38,26 @@ function themeConfigPageFrame() {
 function theme_settings_page() {}
 
 // Customizando o estilo do Admin
-function adminStyles() {
-    wp_enqueue_style('VS Flickr Styles', get_template_directory_uri() . "/admin/style.css");
+function admin_styles() {
+	wp_enqueue_style("VS Flickr Styles", get_template_directory_uri() . "/admin/style.css");
 
-    wp_enqueue_script('VS Flickr Scripts', get_template_directory_uri() . "/admin/script.js");
+	wp_enqueue_script("VS Flickr Scripts", get_template_directory_uri() . "/admin/script.js");
+}
+
+// Post Customizado: Álbum do Flickr
+function custom_post_flickr_album() {
+	$args = array(
+		"public" => true,
+		"label"  => "Albums",
+		"menu_icon" => get_template_directory_uri() . "/admin/album16.png",
+		"supports" => array("title", "editor", "custom-fields", "page-attributes"),
+		"hierarchical" => true
+	);
+	register_post_type("vs_album", $args);
 }
 
 // Adicionando ações ao WP
+add_action("init", "custom_post_flickr_album");
 add_action("admin_menu", "setup_theme_admin_menus");
-add_action('admin_enqueue_scripts', 'adminStyles');
-// add_action('login_enqueue_scripts', 'my_admin_theme_style');
+add_action("admin_enqueue_scripts", "admin_styles");
+// add_action("login_enqueue_scripts", "my_admin_theme_style");
--- flickr/functions.php
+++ flickr/functions.php
@@ -2,49 +2,46 @@
 // Register Custom Taxonomy
 function custom_taxonomy() {
 	$capabilities = array(
-		'assign_terms'					=> 'administrator',
-		'edit_terms'					=> 'administrator',
-		"manage_terms"					=> 'administrator',
-		"delete_terms"					=> 'administrator',
+		"assign_terms"					=> "manage_options",
+		"edit_terms"					=> "manage_options",
+		"manage_terms"					=> "manage_options",
+		"delete_terms"					=> "manage_options",
 	);
 	$labels = array(
-		'name'							=> _x( 'Flickr Albums', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'					=> _x( 'Flickr Album', 'Taxonomy Singular Name', 'text_domain' ),
-		// 'menu_name'						=> __( 'Flickr Albums', 'text_domain' ),
-		// 'all_items'						=> __( 'All Items', 'text_domain' ),
-		// 'parent_item'					=> __( 'Parent Item', 'text_domain' ),
-		// 'parent_item_colon'				=> __( 'Parent Item:', 'text_domain' ),
-		// 'new_item_name'					=> __( 'New Item Name', 'text_domain' ),
-		// 'add_new_item'					=> __( 'Add New Item', 'text_domain' ),
-		// 'edit_item'						=> __( 'Edit Item', 'text_domain' ),
-		// 'update_item'					=> __( 'Update Item', 'text_domain' ),
-		// 'separate_items_with_commas'		=> __( 'Separate items with commas', 'text_domain' ),
-		// 'search_items'					=> __( 'Search Items', 'text_domain' ),
-		// 'add_or_remove_items'			=> __( 'Add or remove items', 'text_domain' ),
-		// 'choose_from_most_used'			=> __( 'Choose from the most used items', 'text_domain' ),
-		// 'not_found'						=> __( 'Not Found', 'text_domain' ),
+		"name"							=> _x( "Flickr Types", "Taxonomy General Name", "text_domain" ),
+		"singular_name"					=> _x( "Flickr Type", "Taxonomy Singular Name", "text_domain" ),
+		// "menu_name"						=> __( "Flickr Albums", "text_domain" ),
+		// "all_items"						=> __( "All Items", "text_domain" ),
+		// "parent_item"					=> __( "Parent Item", "text_domain" ),
+		// "parent_item_colon"				=> __( "Parent Item:", "text_domain" ),
+		// "new_item_name"					=> __( "New Item Name", "text_domain" ),
+		// "add_new_item"					=> __( "Add New Item", "text_domain" ),
+		// "edit_item"						=> __( "Edit Item", "text_domain" ),
+		// "update_item"					=> __( "Update Item", "text_domain" ),
+		// "separate_items_with_commas"		=> __( "Separate items with commas", "text_domain" ),
+		// "search_items"					=> __( "Search Items", "text_domain" ),
+		// "add_or_remove_items"			=> __( "Add or remove items", "text_domain" ),
+		// "choose_from_most_used"			=> __( "Choose from the most used items", "text_domain" ),
+		// "not_found"						=> __( "Not Found", "text_domain" ),
 	);
 	$args = array(
-		'labels'						=> $labels,
-		'hierarchical'					=> true,
-		'public'						=> true,
-		'show_ui'						=> true,
-		'show_admin_column'				=> true,
-		'show_in_nav_menus'				=> true,
-		'show_tagcloud'					=> true,
-		'rewrite'						=> array( 'slug' => 'person' ),
-		'capabilities' 					=> $capabilities,
+		"labels"						=> $labels,
+		"hierarchical"					=> true,
+		"public"						=> false,
+		"show_ui"						=> true,
+		"show_admin_column"				=> true,
+		"show_in_nav_menus"				=> true,
+		"show_tagcloud"					=> true,
+		// "rewrite"						=> array( "slug" => "person" ),
+		"capabilities" 					=> $capabilities,
 	);
-	register_taxonomy( 'flickr_album', array( 'post' ), $args );
+	register_taxonomy("vs_flickr_type", array("vs_album"), $args );
 }
 
-// Hook into the 'init' action
-add_action( 'init', 'custom_taxonomy', 0 );
+// Hook into the "init" action
+add_action( "init", "custom_taxonomy", 0 );
 
 
-
-
-
-
+// FUNÇÕES CUSTOMIZADAS PARA O ADMIN
 if ( is_admin() )
-	include_once get_template_directory() . '/admin/admin-functions.php';
+	include_once get_template_directory() . "/admin/admin-functions.php";
