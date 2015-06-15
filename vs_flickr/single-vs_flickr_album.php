bef56fcdb85d3374fbc637f7dce4c35f45f598bb
Carlos Vinicius <835129+cvscarlos@users.noreply.github.com>
2015-06-15 12:05:11 -0300

    tentando implementar o grid nas fotos

--- vs_flickr/single-vs_flickr_album.php
+++ vs_flickr/single-vs_flickr_album.php
@@ -7,9 +7,10 @@ $albumJson = get_flickr_album($albumId);
 $album = "";
 foreach ($albumJson->photoset->photo as $v) {
 	// $album .= '<li><a href="' . get_site_url() . '/_r/photo/' . sanitize_title($v->title) . '?f_p=' . $v->id . '" title="' . $v->title . '">';
-	$album .= '<li><a href="' . get_site_url() . '?photo=' . sanitize_title($v->title) . '&amp;f_p=' . $v->id . '&amp;_r=1" title="' . $v->title . '">';
+	// $album .= '<li><a href="' . get_site_url() . '?photo=' . sanitize_title($v->title) . '&amp;f_p=' . $v->id . '&amp;_r=1" title="' . $v->title . '">';
+	// $album .= "<img src='https://farm{$v->farm}.staticflickr.com/{$v->server}/{$v->id}_{$v->secret}_m.jpg' alt='{$v->title->_content}' />";
 	$album .= "<img src='https://farm{$v->farm}.staticflickr.com/{$v->server}/{$v->id}_{$v->secret}_m.jpg' alt='{$v->title->_content}' />";
-	$album .= '</a></li>';
+	// $album .= '</a></li>';
 }
 
 get_header();
